458708849b2625d42c90c013a1f9003a21412bfc
Liam Silli <liamsilli@gmail.com>
2025-11-21 18:09:39 -0500

    Added inspection and restaurant methods as well as other changes

---
+++ data/inspections.js
@@ -0,0 +1,59 @@
+// Inspection methods
+/*
+    TODO:
+        add admin methods
+            create, update, remove inspection
+*/
+
+import {users, restaurants, inspections, reviews, comments} from '../config/mongoCollections.js';
+import {ObjectId} from 'mongodb';
+import validation from '../helpers/validation.js';
+import { inspectionData } from './index.js';
+
+let exportedMethods = {
+    async getInspectionById (id) {
+        /* 
+            Inputs: 
+                - id: string of a restaurant's id
+    
+            Purpose: To retrieve a inspection from its id
+    
+            Returns: Inspection object
+        */
+       id = validation.checkId(id);
+
+       const inspectionCollection = await inspections();
+       const inspection = await inspectionCollection.findOne( { _id: new ObjectId(id) } );
+       if (!inspection) throw new Error(`No inspection was found with the id ${id}`);
+       return inspection;
+    },
+    async getInspectionsByRestaurant(restaurantId) {
+        /* 
+            Inputs: 
+                - restaurantId: string of a restaurant's id
+    
+            Purpose: To retrieve the inspections of a restaurant
+    
+            Returns: Array of inspection objects
+        */
+        restaurantId = validation.checkId(restaurantId);
+
+        const inspectionCollection = await inspections();
+        const restaurantCollection = await restaurants();
+
+        //Validate restaurant exists with that id
+        const restaurant = await restaurantCollection.findOne({ _id: new ObjectId(restaurantId) });
+        if (!restaurant) throw new Error('No restaurant found with the id ${id}');
+
+        const result = inspectionCollection.find({ restaurantId: new ObjectId(restaurantId) }).sort({ inspectionDate: -1}).toArray();
+
+    }
+
+
+
+
+
+
+};
+
+export default exportedMethods;
---
+++ data/users.js
@@ -0,0 +1,18 @@
+// User methods
+
+import {users, restaurants, inspections, reviews, comments} from '../config/mongoCollections.js';
+import {ObjectId} from 'mongodb';
+import validation from '../helpers/validation.js';
+
+let exportedMethods = {
+
+
+
+
+
+
+
+
+};
+
+export default exportedMethods;
